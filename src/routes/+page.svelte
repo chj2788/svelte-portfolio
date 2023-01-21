7b6afc5e28af26b060a45c9465a106b5708f60f1
chj2788 <chj2788@gmail.com>
2023-01-20 16:02:57 -0800

    add multiline typewriter effects for welcome page description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -15,9 +15,11 @@
 		<div class="intro-text">I am a </div>
 		<div class="intro-text">Software-</div>
 		<div class="intro-text">Engineer</div>
-		<p class="description">
-			I am a software engineer with a background in data science. Currently, I'm focusing on building a full-stack applications.
-		</p>
+		<span class="description line__1">
+			I am a software engineer with a background
+		</span>
+		<span class="description line__2">in data science. Currently, I'm focusing on</span>
+		<span class="description line__3">building full-stack applications.</span>
 	</div>
 	<div class="canvas-wrapper">
 		<Canvas size={{ width: 500, height: 500 }}>
@@ -34,23 +36,90 @@
 		font-family: 'Syncopate', sans-serif;
 		width: 50%
 	}
-	.canvas-wrapper {
-    /* width: 50%;
-    height: auto; */
-  }
+	/* .canvas-wrapper {
+    width: 50%;
+    height: auto;
+  } */
 
 	.about-wrapper {
 		display: flex;
-		flex-direction: row;
-		width:100%;
+		width:90%;
 		justify-content: center;
 		align-items: center;
-		height: 100%;
+		margin: 8% 5%;
 	}
 
 	.description {
 		font-family: 'Press Start 2P', cursive;
 		color: lightgrey;
+		margin: 1% 0;
+		white-space: nowrap;
+		overflow: hidden;
 	}
+
+	.line__1 {
+		border-right: 2px solid rgba(17, 17, 17, 0.9);
+
+		/* define animation types for text + cursor */
+		animation: animated-text-1 6s steps(30, end) 1s 1 normal both,
+			animated-cursor 900ms steps(30, end) 8;
+	}
+
+	.line__2 {
+		border-right: 2px solid rgba(17, 17, 17, 0.9);
+
+		/* define animation types for text + cursor */
+		animation: animated-text-2 5s steps(30, end) 1s 1 normal both,
+			animated-cursor 900ms steps(30, end) 8;
+		-webkit-animation-delay: 7s;
+		animation-delay: 7s;
+	}
+
+	.line__3 {
+		border-right: 2px solid rgba(17, 17, 17, 0.9);
+
+		/* define animation types for text + cursor */
+		animation: animated-text-3 5s steps(30, end) 1s 1 normal both,
+			animated-cursor 900ms steps(30, end) infinite;
+		-webkit-animation-delay: 7s;
+		animation-delay: 12s;
+	}
+
+	@keyframes animated-cursor {
+		from {
+			border-right-color: rgba(17, 17, 17, 0.9);
+		}
+		to {
+			border-right-color: rgba(255, 255, 255, 0.8);
+		}
+	}
+
+	@keyframes animated-text-1 {
+		from {
+			width: 0;
+		}
+		to {
+			width: 43rem;
+		}
+	}
+
+	@keyframes animated-text-2 {
+		from {
+			width: 0;
+		}
+		to {
+			width: 43rem;
+		}
+	}
+
+	@keyframes animated-text-3 {
+		from {
+			width: 0;
+		}
+		to {
+			width: 33rem;
+		}
+	}
+
 </style>
 
